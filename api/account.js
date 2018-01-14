98c60b6c25fa1e86abc715c8aaae0d7604214704
Justnuked <thommartens14@hotmail.com>
2018-01-14 16:52:26 +0100

    added unittests

--- api/account.js
+++ api/account.js
@@ -5,6 +5,5 @@ var accountController = require('../controllers/account.controller');
 routes.post('/login', accountController.login);
 
 routes.post('/register', accountController.register);
-routes.get('/logintest', accountController.logintest);
 
 module.exports = routes;
