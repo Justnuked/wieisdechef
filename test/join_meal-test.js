0b02b24b1fbef18c51db7931e5a41b30dc9cb07f
Casper // Astral <Cvissers97@users.noreply.github.com>
2018-01-14 16:50:07 +0100

    added authentication to tests

--- test/join_meal-test.js
+++ test/join_meal-test.js
@@ -2,10 +2,30 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../server');
 var chould = chai.should();
+var token = '';
 
 chai.use(chaiHttp);
 
 describe('joinmeal API funtionalities', function() {
+	
+		before(function(done){
+		let	login = {
+			username : 'sjef',
+			password : '123'
+		}
+		chai.request(server)
+			.post('/api/login')
+			.send(login)
+			.end(function(error,	result){
+				result.should.have.status(200);
+				result.should.be.json;
+				result.should.be.an('object');
+				
+				token = result.body.token;
+				done();
+			});
+		
+	});
 	
 	it('should not participate in dinner without dinnerID', function(done) {
 		let participate = {
@@ -15,6 +35,7 @@ describe('joinmeal API funtionalities', function() {
 		chai.request(server)
 			.post('/api/session/joinmeal')
 			.send(participate)
+			.set('X-Access-Token', token)
 			.end(function(error, result) {
 				result.should.have.status(404);
 				result.should.be.json;
@@ -32,6 +53,7 @@ describe('joinmeal API funtionalities', function() {
 		chai.request(server)
 			.post('/api/session/joinmeal')
 			.send(participate)
+			.set('X-Access-Token', token)
 			.end(function(error, result) {
 				result.should.have.status(404);
 				result.should.be.json;
