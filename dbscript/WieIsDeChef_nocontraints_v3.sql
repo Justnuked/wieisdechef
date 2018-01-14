b2a2d103d8328fc470be7364f3f2c6201ca69fbd
Casper // Astral <Cvissers97@users.noreply.github.com>
2018-01-14 15:51:11 +0100

    updated db

---
+++ dbscript/WieIsDeChef_nocontraints_v3.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `students` (`id` int(255) NOT NULL AUTO_INCREMENT, `username` varchar(64) NOT NULL UNIQUE, `password` varchar(5000) NOT NULL, PRIMARY KEY (`id`));
+CREATE TABLE `meals` (`id` int(255) NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `price` double NOT NULL, `maxPeople` int(32) NOT NULL, `image` blob, `description` varchar(255), PRIMARY KEY (`id`));
+CREATE TABLE `dinners` (`id` int(255) NOT NULL AUTO_INCREMENT, `chefId` int(255) NOT NULL, `mealId` int(255) NOT NULL, `date` datetime NOT NULL, PRIMARY KEY (`id`));
+CREATE TABLE `participants` (`id` int(255) NOT NULL AUTO_INCREMENT, `dinnerId` int(255) NOT NULL, `studentId` int(255) NOT NULL, `extras` int(10) DEFAULT 0 NOT NULL, PRIMARY KEY (`id`));
