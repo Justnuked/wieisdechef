99645442c21c6be7a4b047d265c72828d050f27b
Kevin Rademacher <Kevin Rademacher>
2018-01-16 12:18:53 +0100

    Added comments to server and pathfile

--- api/routes.js
+++ api/routes.js
@@ -2,28 +2,52 @@ var express = require('express');
 var routes = express.Router();
 var dbController = require('../controllers/db.controller');
 
-//Joining a meal
+/**
+ * @author: Kevin
+ * @description: Joining a meal
+ */
 routes.post('/joinmeal', dbController.joinMeal);
 
-//Being a chef and hosting a meal
+/**
+ * @author: Kevin
+ * @description: Being a chef and hosting a meal
+ */
 routes.post('/hostmeal', dbController.hostMeal);
 
-//Leaving a meal
+/**
+ * @author: Kevin
+ * @description: Leaving a meal
+ */
 routes.post('/leavemeal', dbController.leaveMeal);
 
-//Adds a new meal
+/**
+ * @author: Kevin
+ * @description: Adds a new meal
+ */
 routes.post('/addmeal', dbController.addMeal);
 
-//Return a single meal
+/**
+ * @author: Kevin
+ * @description: Return a single meal
+ */
 routes.get('/getmeal', dbController.getMeal);
 
-//An overview on all data
+/**
+ * @author: Kevin
+ * @description: An overview on all data
+ */
 routes.get('/overview', dbController.showOverview);
 
-//An overview of all the meals
+/**
+ * @author: Kevin
+ * @description: An overview of all the meals
+ */
 routes.get('/allmeals', dbController.showAllMeals);
 
-//Get all nonexistent endpoints
+/**
+ * @author: Kevin
+ * @description: Returns a 400 error for all non existing endpoints
+ */
 routes.get('*', function(req, res, next) {
 	res.status(404).json({Error: 'Endpoint nonexistent'}).end();
 });
--- server.js
+++ server.js
@@ -17,12 +17,19 @@ app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(logger('dev'));
 
-//Add JSON content type to all.
+/**
+ * @author: Kevin
+ * @description: Adds JSON content type to all.
+ */
 app.use('*', function(req, res, next){
 	res.contentType('application/json');
 	next();
 });
 
+/**
+ * @author: Thom
+ * @description: Checks if user is logged in before accessing everything behind /session/
+ */
 app.use('/api/session/*', function(req, res, next){
 	
 	console.log("VALIDATE TOKEN")
@@ -40,17 +47,24 @@ app.use('/api/session/*', function(req, res, next){
     });
 });
 
-
-
+//Set the file endpoints
 app.use('/api/session', routes);
 app.use('/api', account);
 
+/**
+ * @author: Kevin
+ * @description: Returns a 400 error for all non existing endpoints
+ */
 app.use('*', function(req, res, next){
 	res.status(400);
 	res.json({Error: 'No matching endpoint'});
 	res.end();
 });
 
+/**
+ * @author: Kevin
+ * @description: Returns a 404 error for everything else
+ */
 app.use('*', function(err, req, res, next){
 	console.log('Error: ' + err);
 	res.status(404).json({error: err}).end();
