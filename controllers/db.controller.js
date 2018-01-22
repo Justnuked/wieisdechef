79d19cf1c8fe2b5bae3bf0783dd426af81fdb15c
Kevin Rademacher <Kevin Rademacher>
2018-01-22 01:27:49 +0100

    Changed the getMeal endpoint so that the image is in base64

--- controllers/db.controller.js
+++ controllers/db.controller.js
@@ -86,8 +86,9 @@ module.exports = {
 			if(error) {
 				next(error);
 			} else {
+				var meal = results[0];
 				res.status(200);
-				res.json(results[0]); //Returns the first item in the array.
+				res.json({id: meal.id, name: meal.name, price: meal.price, maxPeople: meal.maxPeople, description: meal.description, image: meal.image.toString('base64')}); //Returns the first item in the array.
 				res.end();
 			}
 		});
